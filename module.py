60b2ab9de17744ec2df939ec3bf248b5309fd779
Noah Rosenbaum <noahrosenbaum762@gmail.com>
2021-07-14 21:08:54 -0500

    Python Questions

---
+++ module.py
@@ -0,0 +1,18 @@
+# def printName(name):
+#     print(f"Hello Mr/Ms {name}...we've been waiting for you!")
+import math
+
+def square(length, width):
+    area = length * width
+    print(area)
+
+square(10,10)
+
+
+
+
+def circumferenceCircle(radius):
+    circumference = 2 * radius * math.pi
+    print(circumference)
+
+circumferenceCircle(20)
